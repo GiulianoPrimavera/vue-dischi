423ac22ded88c2e3cb9d7929ab6930eea0f2470c
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-15 15:44:52 +0100

    adds main files

---
+++ src/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="main">
+  <div class="navbar"></div>
+
+  <div class="cards_container">
+      <SingleCard></SingleCard>
+  </div>
+</div>
+</template>
+
+<script>
+import SingleCard from './SingleCard.vue'
+
+export default {
+    components: {
+        SingleCard
+    }
+};
+</script>
---
+++ src/components/SingleCard.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="single_card"></div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
