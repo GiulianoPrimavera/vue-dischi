371236f04117972347ed2500ab77e7cadceab224
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-15 17:00:05 +0100

    adds elements for the single card

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -1,19 +1,22 @@
 <template>
     <div class="single_card">
-        <div class="single_card_image"></div>
+        <div class="single_card_image">
+            <img :src="image" :alt="title">
+        </div>
 
         <div class="single_card_title">
-            <h2></h2>
+            <h2>{{ title }}</h2>
         </div>
 
         <div class="single_card_description">
-            <p></p>
+            <p>{{ artist }}</p>
+            <p>{{ year }}</p>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    
+    props:["image", "title", "artist", "year"]
 }
 </script>
