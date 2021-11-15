7eb1764ab445f1b4223e531cd2c0a8b54aded2e2
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-15 16:16:40 +0100

    adds single card structure html

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -1,5 +1,15 @@
 <template>
-    <div class="single_card"></div>
+    <div class="single_card">
+        <div class="single_card_image"></div>
+
+        <div class="single_card_title">
+            <h2></h2>
+        </div>
+
+        <div class="single_card_description">
+            <p></p>
+        </div>
+    </div>
 </template>
 
 <script>
