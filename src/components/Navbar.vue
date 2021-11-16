9f99371a5a8d84db9d95f5360b13e1bc6c852414
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-11-16 15:49:56 +0100

    adds js file given to set correctly the files

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,15 @@
 <template>
-   <div class="navbar">
-      <img src="@/assets/spoty.png" alt="">
+  <div class="navbar">
+    <div class="navbar_logo_container">
+      <img src="@/assets/spoty.png" alt="" />
+    </div>
+
+    <div class="navbar_filter_container">
+       <select name="album_genre"></select>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
